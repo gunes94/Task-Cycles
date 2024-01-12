5976a865d9dd9dbc20bdf01a52ffee1664c8fb09
Gyunesh Mehmed <30699247+gunes94@users.noreply.github.com>
2024-01-12 17:37:52 +0200

    Create ReverseNumber.java

---
+++ ReverseNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+//Направете програма, която печата цифрите на въведено от конзолата цяло число в обратен ред.
+//Ако числото е отрицателно - просто игнорирайте знака.
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int reverse = 0;
+
+        if (number < 0){
+            number *= -1;
+        }
+
+        while(number != 0){
+            int reminder = number % 10;
+            reverse = reverse * 10 + reminder;
+            number = number / 10;
+        }
+
+        System.out.println(reverse);
+    }
+}
